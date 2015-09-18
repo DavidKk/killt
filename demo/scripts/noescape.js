d1cf70847f4ed4aa0c8d704e0b1d5014fe7f1429
David Jones <qowera@qq.com>
2015-09-18 10:03:08 +0800

    demo 向下兼容

--- demo/scripts/noescape.js
+++ demo/scripts/noescape.js
@@ -1,24 +1,28 @@
 window.onload = function() {
   document.body.innerHTML = oTemplate.renderById('/template/noescape.html', {
     title: 'oTemplate',
-    noescapeContent: '<div>\
-        Check it out!!!\
-        <ul>\
-          <li>check</li>\
-          <li>check</li>\
-          <li>check</li>\
-          <li>yoyoyo</li>\
-        </ul>\
-      </div>',
-    escapeContent: '<div>\
-        Check it out!!!\
-        <ul>\
-          <li>check</li>\
-          <li>check</li>\
-          <li>check</li>\
-          <li>yoyoyo</li>\
-        </ul>\
-      </div>'
+    noescapeContent: [
+        '<div>',
+        '   Check it out!!!',
+        '   <ul>',
+        '     <li>check</li>',
+        '     <li>check</li>',
+        '     <li>check</li>',
+        '     <li>yoyoyo</li>',
+        '   </ul>',
+        '</div>'
+      ].join('\n')
+    escapeContent: [
+        '<div>',
+        '   Check it out!!!',
+        '   <ul>',
+        '     <li>check</li>',
+        '     <li>check</li>',
+        '     <li>check</li>',
+        '     <li>yoyoyo</li>',
+        '   </ul>',
+        '</div>'
+      ].join('\n')
   }, {
     escape: true
   })
