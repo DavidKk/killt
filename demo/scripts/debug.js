63b2011f14a803be21e3c59f7c9baf6004601a09
David Jones <qowera@qq.com>
2015-12-19 17:40:08 +0800

    no message

--- demo/scripts/debug.js
+++ demo/scripts/debug.js
@@ -2,7 +2,6 @@ window.onload = function() {
   oTemplate.config('env', oTemplate.ENV.UNIT)
 
   oTemplate.onError(function(error, message) {
-    console.log(arguments)
     message = oTemplate.helper('$escape')(message)
     document.body.innerHTML += message.replace(/\n/g, '<br>')
     console.log && console.log(message)
